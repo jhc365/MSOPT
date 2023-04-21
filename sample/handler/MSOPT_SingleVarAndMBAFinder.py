946a89340e70a71e55216da366bd78a96976a1f2
jhc <jk365a@naver.com>
2023-04-21 14:32:31 +0900

    --MSOPT step1,step2 complete

--- sample/handler/MSOPT_SingleVarAndMBAFinder.py
+++ sample/handler/MSOPT_SingleVarAndMBAFinder.py
@@ -6,15 +6,25 @@ def check_single_variable_expression(expr, rawexpr, id, singleOpfile) -> bool:
     #expr을 받아 메모리 변수 혹은 레지스터 하나만으로 이루어진 expr인지 검사
     #검사 후 singlevar이면 파일에 write 후 bool 반환
 
-    if expr.is_mem() | expr.is_id():
+    if expr.is_mem() | expr.is_id(): #single var
         str1 = "trace%s:%s\n" % (id, str(rawexpr))
         str2 = "    SE:%s\n" % (str(expr))
         singleOpfile.write(str1)
         singleOpfile.write(str2)
+        singleOpfile.write("\n")
         return True
-    elif is_expr(expr):
-        if (expr.args[0].is_mem() | expr.args[0].is_id()) | (expr.args[1].is_mem() | expr.args[1].is_id()):
-            print("a\na\na\n")
+    elif expr.is_op(): #exprOp
+        # single op일 경우(@32[...] == 0x....)
+        #(양 변이 모두 MEM,ID,INT 등 단일 변수, 상수일 경우)
+
+        if (expr.args[0].is_mem() | expr.args[0].is_id() | expr.args[0].is_int()) & \
+                (expr.args[1].is_mem() | expr.args[1].is_id() | expr.args[1].is_int()):
+            str1 = "trace%s:%s\n" % (id, str(rawexpr))
+            str2 = "    SE:%s\n" % (str(expr))
+            singleOpfile.write(str1)
+            singleOpfile.write(str2)
+            singleOpfile.write("\n")
+            return True
     else:
         return False
 
@@ -26,12 +36,13 @@ def check_MBA_and_writefiles(expr, rawexpr, id, f_mba, f_nmba , size = 32) -> bo
 
     p1 = re.compile("[\+\-\*\/\<\>\^]") # arithmetic and bits ops
     p2 = re.compile("[\&\|]") # boolean ops
-    print(expr)
+
     if (len(p1.findall(str(expr))) != 0) & (len(p2.findall(str(expr))) != 0):
         str1 = "trace%s:%s\n" % (id, str(rawexpr))
-        str2 = "    SE:%s\n" %(str(expr))
+        str2 = "    SE:%s\n" % (str(expr))
         f_mba.write(str1)
         f_mba.write(str2)
+        f_mba.write("\n")
 
         return True
     else:
@@ -39,6 +50,7 @@ def check_MBA_and_writefiles(expr, rawexpr, id, f_mba, f_nmba , size = 32) -> bo
         str2 = "    SE:%s\n" % (str(expr))
         f_nmba.write(str1)
         f_nmba.write(str2)
+        f_nmba.write("\n")
 
         return False
 
@@ -60,7 +72,7 @@ def string2ExprOp_list_with_MSOPT_classify(strings, size = 32):
     for s in strings:
         raw = s
         outcode = eval(raw)
-        if not check_single_variable_expression(outcode, raw, strings[s], singleOpfile):
-            if not check_MBA_and_writefiles(outcode, raw, strings[s],f_mba, f_nmba, 32):
-                yield strings[s],outcode,"vm"
+        if not check_single_variable_expression(outcode, raw, strings[s], singleOpfile): #not singleVar만 통과
+            if check_MBA_and_writefiles(outcode, raw, strings[s],f_mba, f_nmba, 32): #MBA만 통과
+                yield strings[s],outcode,"vm" #mba 샘플만 반환
     # return output_dict
